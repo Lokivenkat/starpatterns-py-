b4c3c8fefb34125c22d256e20f326dd1ea3f3ea1
Loki venkat <69497567+Lokivenkat@users.noreply.github.com>
2021-10-14 22:05:08 +0530

    Add files via upload

---
+++ basicstar.py
@@ -0,0 +1,5 @@
+n=5
+for i in range(n):
+    for j in range(n):
+        print('*',end=' ')
+    print()
---
+++ reversehill.py
@@ -0,0 +1,9 @@
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end=' ')
+    for j in range(i,n-1):
+        print('*',end=' ')
+    for j in range(i,n):
+        print('*',end=' ')
+    print()
